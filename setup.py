dba822021232d02ed4c4fd2c61c96c6c97a2b8e2
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2017-02-05 15:04:43 +0100

    change name too joob

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(
+    name='joob',
+    packages=['joob'],
+    version='0.1.0',
+    description='A lightweight background task runner',
+    author='Mahdi Zareie',
+    author_email='mahdi.elf@gmail.com',
+    url='https://github.com/inb-co/poseidon',
+    keywords=['worker', 'task runner', 'lightweight job runner'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
