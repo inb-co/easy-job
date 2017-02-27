83791b2d89b1ef077570beb0d045b6ef8860a8f0
Mahdi Zareie <mahdi.elf@gmail.com>
2017-02-27 11:14:55 +0330

    Feature: add an empty callback function

--- easy_job/workers/common.py
+++ easy_job/workers/common.py
@@ -5,3 +5,7 @@ __author__ = "Apollo"
 
 def call_with_retry(function, args: tuple, kwargs: dict, retry_policy: dict):
     return Retrying(**retry_policy).call(function, *args, **kwargs)
+
+
+def empty_callback(result, logger, *args, **kwargs):
+    return result
